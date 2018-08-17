203ce42ea3752835add2aa6dd19197558e3b398b
nathias <n@th1.as>
2018-08-17 13:46:36 +0200

    endsWith("!")

---
+++ endsWith.js
@@ -0,0 +1,10 @@
+// var name = prompt("What is your name? ");
+var name = 'Billiiiee!';
+
+//if (name[name.length - 1] === '!') {
+if (name.endsWith('!')) {
+  console.log("HELLO" + ' ' + name.toUpperCase() + '. WHY ARE WE SCREAMING?');
+} else {
+  console.log("Hello" + ' ' + name);
+}
+
