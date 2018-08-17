0724ae7500b80c3e8e779ab0b4d6eda97129a9cc
nathias <n@th1.as>
2018-08-17 21:03:15 +0200

    5

---
+++ regex/5jsregex.js
@@ -0,0 +1,20 @@
+function danish(string) {
+  return string.replace(/\bapple|cherry|blueberry\b/, 'danish');
+}
+
+console.log(
+danish('An apple a day keeps the doctor away'),
+// -> 'An danish a day keeps the doctor away'
+
+danish('My favorite is blueberry pie'),
+// -> 'My favorite is danish pie'
+
+danish('The cherry of my eye'),
+// -> 'The danish of my eye'
+
+danish('apple. cherry. blueberry.'),
+// -> 'danish. cherry. blueberry.'
+
+danish('I love pineapple'),
+// -> 'I love pineapple'
+);
