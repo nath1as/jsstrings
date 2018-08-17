d4fc31f254ddd0162c7fa8f0ab72dbd3e16a3d71
nathias <n@th1.as>
2018-08-17 13:19:06 +0200

    lastIndexOf

---
+++ lastIndex.js
@@ -0,0 +1,6 @@
+var language = 'JavaScript';
+var idx = language.indexOf('S');
+var charCode = language.charCodeAt(idx);
+var string = String.fromCharCode(charCode);
+var lastIndex = language.lastIndexOf('a');
+console.log(lastIndex);
