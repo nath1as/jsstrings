40a0ff0d1ee5cf90229a8f71b075086c6aa0de23
nathias <n@th1.as>
2018-08-17 13:23:10 +0200

    substr

---
+++ substr.js
@@ -0,0 +1,7 @@
+var language = 'JavaScript';
+var aIndex = language.indexOf('a');
+var vIndex = language.indexOf('v');
+var subA = language.substr(aIndex, 4);
+var subV = language.substr(vIndex, 4);
+console.log(subA);
+console.log(subV);
