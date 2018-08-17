20d5d66dd4638f68ab85616c4acf9612e0cd892d
nathias <n@th1.as>
2018-08-17 20:22:19 +0200

    10

---
+++ regex/10quantifiers.js
@@ -0,0 +1,11 @@
+var string = `
+123,456,789,123,345
+123,456,,789,123
+23,56,7
+13,45,78,23,45,34
+13,45,78,23,45,34,56
+`;
+
+var match = string.match(/^(\d+,){2,5}\d+$/gim);
+console.log(match);
+console.log(match.length);
