b1a6cbb80942b6116a9302102b6e128d7af6a868
nathias <n@th1.as>
2018-08-17 13:24:42 +0200

    substring

---
+++ substring.js
@@ -0,0 +1,7 @@
+var language = 'JavaScript';
+var aIndex = language.indexOf('a');
+var vIndex = language.indexOf('v');
+var subA = language.substring(aIndex, 4);
+var subV = language.substring(vIndex, 4);
+console.log(subA);
+console.log(subV);
