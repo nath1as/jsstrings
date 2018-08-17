97c423b5730ff72b57e4390af39d58bada88901f
nathias <n@th1.as>
2018-08-17 13:29:51 +0200

    brackets

---
+++ bracket.js
@@ -0,0 +1,5 @@
+var fact1 = 'JavaScript is fun';
+var fact2 = 'Autists like it too';
+var compoundSentence = fact1 + ' and ' + fact2.toLowerCase();
+console.log(fact1[0], fact2[0]);
+
---
+++ toLowerCase.js
@@ -0,0 +1,5 @@
+var fact1 = 'JavaScript is fun';
+var fact2 = 'Autists like it too';
+var compoundSentence = fact1 + ' and ' + fact2.toLowerCase();
+console.log(compoundSentence);
+
