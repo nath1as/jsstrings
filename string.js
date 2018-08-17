5f33a2fb4cb218afdd96c90ef51839d04c3039dc
nathias <n@th1.as>
2018-08-17 13:40:04 +0200

    String() method

---
+++ string.js
@@ -0,0 +1,6 @@
+var boxNumber = (356).toString();
+console.log(typeof boxNumber);
+boxNumber = parseInt(boxNumber, 10);
+console.log(typeof boxNumber);
+boxNumber = String(boxNumber);
+console.log(typeof boxNumber);
