9129aaa1054b6236ce40ead4d2b64c6c9b055c4a
nathias <n@th1.as>
2018-08-18 16:32:21 +0200

    capitalize words

---
+++ capitalizeWords.js
@@ -0,0 +1,11 @@
+function wordCap(string) {
+  return string.split(' ').map(function (word) {
+    return word[0].toUpperCase() + word.slice(1).toLowerCase();
+  }).join(' ');
+}
+
+console.log(
+  wordCap('four score and seven'),       // "Four Score And Seven"
+  wordCap('the javaScript language'),    // "The Javascript Language"
+  wordCap('this is a "quoted" word'),    // 'This Is A "quoted" Word'
+);
