c92e0951d5273fdc285ab3f31bdda66a22c3411c
nathias <n@th1.as>
2018-08-18 12:19:21 +0200

    reverse string

---
+++ reverseString.js
@@ -0,0 +1,7 @@
+function reverse(string) {
+  return string.split('').reverse().join('');
+}
+
+console.log(reverse('hello'));                  // returns "olleh"
+console.log(reverse('The quick brown fox'));    // returns "xof nworb kciuq ehT"
+
